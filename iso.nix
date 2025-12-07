c4a6ca23d621bf35850e694023559300af14060d
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2025-12-07 15:30:13 +0000

    Generate ISO.

--- iso.nix
+++ iso.nix
@@ -21,19 +21,23 @@ services.displayManager.defaultSession = "xfce";
 services.displayManager.autoLogin.enable = true;
 services.displayManager.autoLogin.user = "demos";
 
-users.users.demos.isNormalUser = true;
+users.users.demos = {
+  isNormalUser = true;
+  password = "demos";
+};
 nixpkgs.config.pulseaudio = true;
 
 environment.systemPackages = with pkgs; [
-  python313
-  python313Packages.pygame
+  (python313.withPackages (ps: with ps; [ pygame pytmx cairosvg ]))
   jre
 ];
+
+environment.etc."xdg/autostart/xfce4-hide-icons.desktop".source = ./xfce4-hide-icons.desktop;
 
 system.activationScripts.populateDemo = ''
   cp -r ${./demos} /demos
   cp -r ${./desktop} '/home/demos/Área de Trabalho'
   chown -R demos:users /demos '/home/demos/Área de Trabalho'
-  cp ${./xfce4-desktop.xml} /home/demos/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml
+  chmod -R a+rwX /demos
 '';
 }
